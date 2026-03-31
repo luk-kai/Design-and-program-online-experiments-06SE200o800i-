bd2b75d8cdb3bcbb7c430546eb8b96cbba410a52
Lukas Kaiser <lukaselias.kaiseruzh.ch>
2026-03-31 20:18:19 +0200

    add eligibilityCheck function in hello.js and 3 checks

--- Session4/Lukas_K/hello.js
+++ Session4/Lukas_K/hello.js
@@ -1,7 +1,39 @@
 /* this is…hello.js
-<- this is JS comment
+    this is JS comment
 */
 
 // Method 2 <- this is also JS comment
 
 console.log("Hello, World 2!")
+
+function eligibilityCheck(ageInput, hasParticipatedBefore, hasNormalVisionOrNot, hasCognitiveImpairment) {
+    if (ageInput >= 18 && ageInput <= 35 && !hasParticipatedBefore && hasNormalVisionOrNot && !hasCognitiveImpairment) {
+        console.log("Participant is eligible for the experiment.");
+    } else {
+        console.log("Participant is not eligible for the experiment!");
+    }
+}
+
+ let ageInput = 25; // or 17                                         
+    let hasParticipatedBefore = false; // or True
+    let hasNormalVisionOrNot = true;
+    let hasCognitiveImpairment = false;
+    console.log("age:", ageInput, "participated before?", hasParticipatedBefore, "normal vision:",hasNormalVisionOrNot, "cognitive impairment:",hasCognitiveImpairment)
+    // will this be eligible?
+eligibilityCheck(ageInput, hasParticipatedBefore, hasNormalVisionOrNot, hasCognitiveImpairment);
+
+    ageInput = 17;                                         
+    hasParticipatedBefore = false; // or True
+    hasNormalVisionOrNot = true;
+    hasCognitiveImpairment = false;
+    console.log("age:", ageInput, "participated before?", hasParticipatedBefore, "normal vision:",hasNormalVisionOrNot, "cognitive impairment:",hasCognitiveImpairment)
+    // will this be eligible?
+    eligibilityCheck(ageInput, hasParticipatedBefore, hasNormalVisionOrNot, hasCognitiveImpairment);
+
+    ageInput = 25;                                         
+    hasParticipatedBefore = true; 
+    hasNormalVisionOrNot = true;
+    hasCognitiveImpairment = false;
+    console.log("age:", ageInput, "participated before?", hasParticipatedBefore, "normal vision:",hasNormalVisionOrNot, "cognitive impairment:",hasCognitiveImpairment)
+    // will this be eligible?
+eligibilityCheck(ageInput, hasParticipatedBefore, hasNormalVisionOrNot, hasCognitiveImpairment);

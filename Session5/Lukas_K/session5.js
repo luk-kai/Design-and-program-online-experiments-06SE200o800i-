92bc07dbc491a5c1c500a36384a4cca4b470bc91
Lukas Kaiser <lukaselias.kaiser@uzh.ch>
2026-04-13 11:16:49 +0200

    add for...in and while

--- Session5/Lukas_K/session5.js
+++ Session5/Lukas_K/session5.js
@@ -25,3 +25,15 @@ for (const time of responseTimes) {
 console.log(`Response Time: ${time} ms`);
 }
 
+const trial = { id: 1, responseTime: 350, correct: true };
+console.log(trial.id);
+console.log(trial["id"]);// this is another way to get value of id
+for (const key in trial) {
+console.log(`${key}: ${trial[key]}`);
+}
+
+let i = 0; // Initialize the counter
+while (i < 5) {
+console.log(`Number: ${i}`);
+i++; // Increment the counter
+}
